ea3efcc01d72c069ff58c54c4c6d1559c6390a2f
Matthew Myers <matmyers@umich.edu>
2019-06-20 10:09:49 -0400

    idk for now

---
+++ assets/js/audio.js
@@ -0,0 +1,32 @@
+/*
+	Forty by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var bgm = new Audio('./audio/composition_quest.mp3');
+	var airhorn = new Audio('./audio/airhorn.mp3');
+
+	var	$window = $(window),
+		$body = $('body'),
+		$wrapper = $('#wrapper'),
+		$header = $('#header'),
+		$banner = $('#banner');
+
+		$body.on('click', function(event) {
+
+			bgm.play();
+
+		});
+
+		// $link.on('click', function(event) {
+		// 	airhorn.play();
+
+		// 	// Prevent default.
+		// 		event.stopPropagation();
+		// 		event.preventDefault();
+		// });
+
+})(jQuery);
